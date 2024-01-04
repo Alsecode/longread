b3604798a8c56ed067a5ffa46f9a5626e57e0fda
Artur M <adementor@yandex.ru>
2024-01-04 21:11:41 +0300

    added dark-theme

--- index.js
+++ index.js
@@ -23,3 +23,41 @@ const swiper = new Swiper('.blog-slider', {
       clickable: true,
     }
   });
+
+const switchers = document.querySelectorAll('.change-theme');
+
+switchers.forEach(switcher => {
+  switcher.addEventListener('click', function() {
+    applyTheme(this.dataset.theme);
+    localStorage.setItem('theme', this.dataset.theme);
+  })
+})
+
+const applyTheme = (themeName) => {
+  const themeUrl = `./css/themes/${themeName}.css`;
+
+  const container = document.querySelector('.theme');
+  const themeIcon = document.querySelectorAll('.change-theme');
+  setTimeout(() => {
+    themeIcon.forEach(icon => icon.classList.remove("change"));
+  }, 300);
+  themeIcon.forEach(icon => icon.classList.add("change"));
+
+  if (themeName === 'dark') {
+    container.classList.remove('shadow-dark');
+    container.classList.add('shadow-light');
+  } else {
+    container.classList.add('shadow-dark');
+    container.classList.remove('shadow-light');
+  }
+
+  document.querySelector('[title="theme"]').setAttribute('href', themeUrl);
+  
+}
+
+const activeTheme = localStorage.getItem('theme');
+if (activeTheme === null) {
+  applyTheme('dark');
+} else {
+  applyTheme(activeTheme);
+}
