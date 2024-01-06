af7946dc660d36b0edcd59d4fb70e40a338a0693
Artur M <adementor@yandex.ru>
2024-01-06 04:50:06 +0300

    added contents-menu

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ window.addEventListener('scroll', function() {
     });
  });
 
+// Слайдер
 const swiper = new Swiper('.blog-slider', {
     spaceBetween: 30,
     effect: 'fade',
@@ -24,6 +25,7 @@ const swiper = new Swiper('.blog-slider', {
     }
   });
 
+  // Переключение тем
 const switchers = document.querySelectorAll('.change-theme');
 
 switchers.forEach(switcher => {
@@ -36,20 +38,25 @@ switchers.forEach(switcher => {
 const applyTheme = (themeName) => {
   const themeUrl = `./css/themes/${themeName}.css`;
 
-  const container = document.querySelector('.theme');
-  const themeIcon = document.querySelectorAll('.change-theme');
+  const containers = document.querySelectorAll('.icon');
+  const themeIcon = document.querySelectorAll('.change-icon');
   setTimeout(() => {
     themeIcon.forEach(icon => icon.classList.remove("change"));
   }, 300);
   themeIcon.forEach(icon => icon.classList.add("change"));
 
-  if (themeName === 'dark') {
-    container.classList.remove('shadow-dark');
-    container.classList.add('shadow-light');
-  } else {
-    container.classList.add('shadow-dark');
-    container.classList.remove('shadow-light');
-  }
+  containers.forEach(container => {
+    const classList = container.classList;
+
+    for (var i = classList.length - 1; i >= 0; i -= 1) {
+      if (classList[i].startsWith('shadow')) {
+          classList.remove(classList[i]);
+      }
+    }
+  });
+  containers.forEach(container => {
+    themeName === 'dark' ? container.classList.add(`shadow-light`) : container.classList.add(`shadow-dark`);
+  });
 
   document.querySelector('[title="theme"]').setAttribute('href', themeUrl);
   
@@ -61,3 +68,46 @@ if (activeTheme === null) {
 } else {
   applyTheme(activeTheme);
 }
+
+// Появление иконки с оглавлением
+document.addEventListener('DOMContentLoaded', function () {
+  const icon = document.getElementById('bookmark-icon');
+  const content = document.getElementById('introduction');
+
+  function checkIconVisibility() {
+    const contentTop = content.offsetTop;
+
+    if (window.scrollY >= contentTop) {
+      icon.classList.add('visible');
+    } else {
+      icon.classList.remove('visible');
+    }
+  }
+
+  window.addEventListener('scroll', checkIconVisibility);
+
+  checkIconVisibility();
+});
+
+const openBtns = document.querySelectorAll('.open-modal');
+const modal = document.getElementById('modal');
+console.log(modal);
+
+openBtns.forEach(btn => {
+  btn.addEventListener('click', function() {
+    modal.classList.remove('w-0');
+    const overlay = document.createElement("div");
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+  })
+});
+
+const closeBtn = document.querySelector('.close');
+
+closeBtn.addEventListener('click', function() {
+  modal.classList.add('w-0');
+  const overlay = document.querySelector('.overlay');
+  if (overlay) {
+    document.body.removeChild(overlay);
+  }
+})
