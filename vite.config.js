563f133af63cb5653180f5db6088e89012e2fb28
Artur M <adementor@yandex.ru>
2024-01-29 19:44:48 +0300

    added config for themes change

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        lightTheme: './src/themes/light.css',
+        darkTheme: './src/themes/dark.css',
+      },
+    },
+  },
+};
